5b6a11be803ba34541e735bf376c241b366b1660
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-04-04 08:53:31 +0200

    add oauth2

---
+++ src/main/java/com/softarc/eternal/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.softarc.eternal;
+
+import io.netty.handler.codec.http.HttpMethod;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+      .cors()
+      .and()
+      .authorizeHttpRequests(authorize -> {
+        try {
+          authorize
+            .anyRequest()
+            .authenticated()
+            .and()
+            .oauth2ResourceServer()
+            .jwt();
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+      });
+    return http.build();
+  }
+}
